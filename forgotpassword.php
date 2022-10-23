cd296ac727b95c6a92d347f87ec192c95d1c37dd
Melinigounder <95341802+Melinigounder@users.noreply.github.com>
2022-10-23 14:28:04 +0200

    added a php folder

---
+++ forgotpassword.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+$_SESSION["userId"] = "1";
+$conn = mysqli_connect("localhost", "root", "root", "library") or die("Connection Error: " . mysqli_error($conn));
+
+if (count($_POST) > 0) {
+    $result = mysqli_query($conn, "SELECT *from users WHERE userId='" . $_SESSION["userId"] . "'");
+    $row = mysqli_fetch_array($result);
+    if ($_POST["currentPassword"] == $row["password"]) {
+        mysqli_query($conn, "UPDATE users set password='" . $_POST["newPassword"] . "' WHERE userId='" . $_SESSION["userId"] . "'");
+        $message = "Password Changed";
+    } else
+        $message = "Current Password is not correct";
+}
+?>
+<html>
+<head>
+<title>Change Password</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+<script>
+function validatePassword() {
+var currentPassword,newPassword,confirmPassword,output = true;
+
+currentPassword = document.frmChange.currentPassword;
+newPassword = document.frmChange.newPassword;
+confirmPassword = document.frmChange.confirmPassword;
+
+if(!currentPassword.value) {
+	currentPassword.focus();
+	document.getElementById("currentPassword").innerHTML = "required";
+	output = false;
+}
+else if(!newPassword.value) {
+	newPassword.focus();
+	document.getElementById("newPassword").innerHTML = "required";
+	output = false;
+}
+else if(!confirmPassword.value) {
+	confirmPassword.focus();
+	document.getElementById("confirmPassword").innerHTML = "required";
+	output = false;
+}
+if(newPassword.value != confirmPassword.value) {
+	newPassword.value="";
+	confirmPassword.value="";
+	newPassword.focus();
+	document.getElementById("confirmPassword").innerHTML = "not same";
+	output = false;
+} 	
+return output;
+}
+</script>
+</head>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Added Books</title>
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="css/main.css">
+
+
+<body>
+    <form name="frmChange" method="post" action=""
+        onSubmit="return validatePassword()">
+        <div style="width: 500px;">
+            <div class="message"><?php if(isset($message)) { echo $message; } ?></div>
+            <table border="0" cellpadding="10" cellspacing="0"
+                width="500" align="center" class="tblSaveForm">
+                <tr class="tableheader">
+                    <td colspan="2">Change Password</td>
+                </tr>
+                <tr>
+                    <td width="40%"><label>Current Password</label></td>
+                    <td width="60%"><input type="password"
+                        name="currentPassword" class="txtField" /><span
+                        id="currentPassword" class="required"></span></td>
+                </tr>
+                <tr>
+                    <td><label>New Password</label></td>
+                    <td><input type="password" name="newPassword"
+                        class="txtField" /><span id="newPassword"
+                        class="required"></span></td>
+                </tr>
+                <td><label>Confirm Password</label></td>
+                <td><input type="password" name="confirmPassword"
+                    class="txtField" /><span id="confirmPassword"
+                    class="required"></span></td>
+                </tr>
+                <tr>
+                    
+                        <td><a href="index.php">Submit</a></td>   
+</div>
+                </tr>
+            </table>
+        </div>
+    </form>
+    
+</body>
+</html>
